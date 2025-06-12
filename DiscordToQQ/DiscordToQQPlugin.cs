10f128a41be3cd295a7e3f0a91be8e7f4a0020ce
Nan_Ting <a3380991070@163.com>
2025-06-12 11:15:37 +0800

    DiscordToQQ : Split Method

--- DiscordToQQ/DiscordToQQPlugin.cs
+++ DiscordToQQ/DiscordToQQPlugin.cs
@@ -4,6 +4,7 @@ using NapCatScript.Core;
 using NapCatScript.Core.JsonFormat;
 using NapCatScript.Core.JsonFormat.Msgs;
 using NapCatScript.Core.Model;
+using NapCatScript.Core.MsgHandle;
 using NapCatScript.Core.Services;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -38,7 +39,7 @@ public class DiscordToQQPlugin : PluginType
             Config.SetConf(CURRMAXID, "0");
         }
 
-        GoToGroups.AddRange(GetValues(groups));
+        GoToGroups.AddRange(GetSplitValues(groups));
 
         _ = Task.Run(MsgToQQ);
     }
@@ -61,80 +62,79 @@ public class DiscordToQQPlugin : PluginType
     private bool init = false;
     private async Task MsgToQQ()
     {
-        var getApi = "https://discord.com/api/v9/channels/369115362186231809/messages?limit=50";
-        var htc = new HttpClient();
-        htc.DefaultRequestHeaders.Add("Authorization", Authorization);
-
         while (true) {
             if (init == false) {
                 init = true;
             } else {
                 await Task.Delay(TimeSpan.FromMinutes(30));
             }
-            var getRespMsg = await htc.GetAsync(getApi);
-            var msgJson = await getRespMsg.Content.ReadAsStringAsync();
-            if (msgJson is null)
-                return;
-
-            if (!msgJson.GetJsonElement(out var je))
-                return;
-
-            //遍历消息 并载入
-            foreach (var item in je.EnumerateArray().AsEnumerable()) {
-                try {
-                    var obj = item.Deserialize<DiscordMessage>();
-                    if (obj is null)
-                        continue;
-
-                    if (oldMsgs.Any(f => f.Id == obj.Id))
-                        continue;
-
-                    oldMsgs.Add(obj);
-                } catch (Exception e) {
-                    Log.InstanceLog.Erro("消息序列化失败！", e.Message, e.StackTrace);
-                }
-            }
+            await GetMsgAddToList(oldMsgs);
 
             //按照时间排序
             oldMsgs = oldMsgs.OrderBy(dmsg => dmsg.Id).ToList();
-
             var count = oldMsgs.Count;
             if (count > 50) {
                 oldMsgs = oldMsgs.TakeLast(50).ToList();
             }
-
             if (count <= 0)
                 return;
 
             //转发最新消息
             var sendMsg = oldMsgs[^1];
 
-            if (!ulong.TryParse(sendMsg.Id, out ulong newid))
-                continue;
-
-            if (!ulong.TryParse(maxMsgId, out ulong oldid))
-                continue;
-
-            if (!(newid > oldid))
-                continue;
-
-            Log.Info("Discord 有最新消息！/ Discord new message!");
-            foreach (var groupId in GoToGroups) {
-                Send.SendMsg(groupId, MsgTo.group, new TextJson(sendMsg.Content));
+            if (IsYesMsg(sendMsg)) 
+            {
+                Log.Info("Discord 有最新消息！/ Discord new message!");
+                foreach (var groupId in GoToGroups) {
+                    Send.SendMsg(groupId, MsgTo.group, new TextJson(sendMsg.Content));
+                }
+                maxMsgId = sendMsg.Id;
+                Config.SetConf(CURRMAXID, sendMsg.Id);
             }
-            maxMsgId = sendMsg.Id;
-            Config.SetConf(CURRMAXID, sendMsg.Id);
         }
     }
 
-    private static string[] GetValues(string? str)
+    private const string getApi = "https://discord.com/api/v9/channels/369115362186231809/messages?limit=50";
+    private readonly HttpClient htc = new HttpClient();
+    private async Task GetMsgAddToList<T>(List<T> oldMsgs) where T : DiscordMessage
+    {
+        htc.DefaultRequestHeaders.Add("Authorization", Authorization);
+        var getRespMsg = await htc.GetAsync(getApi);
+        var msgJson = await getRespMsg.Content.ReadAsStringAsync();
+        if (msgJson is null ||
+            !msgJson.GetJsonElement(out var je)) {
+            return;
+        }
+
+        //遍历消息 并加入到集合
+        foreach (var item in je.EnumerateArray().AsEnumerable()) {
+            try {
+                var obj = item.Deserialize<T>();
+                if (!(obj is null ||
+                    oldMsgs.Any(f => f.Id == obj.Id))) {
+                    oldMsgs.Add(obj);
+                }
+            } catch (Exception e) {
+                Log.InstanceLog.Erro("消息序列化失败！", e.Message, e.StackTrace);
+            }
+        }
+    }
+
+    private static string[] GetSplitValues(string? str, char splitChar = ',')
     {
         if (str == null)
             return [];
-        var values = str.Split(",");
+        var values = str.Split(splitChar);
         for (int i = 0; i < values.Length; i++) {
             values[i] = values[i].Trim();
         }
         return values;
+    }
+
+    private bool IsYesMsg(DiscordMessage msg)
+    {
+        return ulong.TryParse(msg.Id, out ulong newid)      &&
+               ulong.TryParse(maxMsgId, out ulong oldid)    &&
+               newid > oldid;
     }
 }
